9596857a724078dc3578e291ada64bc30b193032
Matheus Barbosa <matheuskleber@live.com>
2021-10-04 15:04:08 -0300

    bootstrap project

---
+++ go.mod
@@ -0,0 +1,7 @@
+module go-chat
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Print("Server starting at localhost:4444")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
