fd9b8e3a06f29eb2cedf4f0a7f79a2a8fd4e2c62
Matheus Barbosa <matheuskleber@live.com>
2021-10-04 16:10:35 -0300

    feat(front): seta front basico

--- public/app.js
+++ public/app.js
@@ -1,8 +1,26 @@
 window.addEventListener('DOMContentLoaded', (_) => {
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      let username = document.getElementById("input-username");
-      let text = document.getElementById("input-text");
-      text.value = "";
-    });
-   });
+  let websocket = new WebSocket("ws://" + window.location.host + "/websocket");
+  let room = document.getElementById("chat-text");
+
+  websocket.addEventListener("message", function (e) {
+    let data = JSON.parse(e.data);
+    let chatContent = `<p><strong>${data.username}</strong>: ${data.text}</p>`;
+
+    room.append(chatContent);
+    room.scrollTop = room.scrollHeight; // Auto scroll to the bottom
+  });
+
+  let form = document.getElementById("input-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("input-username");
+    let text = document.getElementById("input-text");
+    websocket.send(
+      JSON.stringify({
+        username: username.value,
+        text: text.value,
+      })
+    );
+    text.value = "";
+  });
+});
